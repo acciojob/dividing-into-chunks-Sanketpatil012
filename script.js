9a495111f1cfd0f3e16fdb929707b6954c63ef58
Sanketpatil012 <122162948+Sanketpatil012@users.noreply.github.com>
2025-02-27 17:19:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+  let result = [] 
+  let subArray= []
+
+ let  currentSum=0; 
+  
+  for(let num of arr){
+    if(currentSum + num >= n){
+		subArray.push(num)
+		currentSum+=num
+	}else{
+		 result.push(subArray)
+		 subArray=[num] 
+		currentSum=num
+		 
+	}
+  }
+	if(arr.length >  0){
+		result.push(subArray)
+	}
+	 return result 
 };
 
-const n = prompt("Enter n: ");
+//const n = prompt("Enter n: ");
 alert(JSON.stringify(divide(arr, n)));
